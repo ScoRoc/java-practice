fce659ce22fae70ce37e356a770a0050a74d854b
Scott Rosehart <srosehart@zulily.com>
2019-04-12 12:49:07 -0700

    git init

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class HelloWorld {
+    public static void TestMethod() {
+        System.out.println("yooo");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("again.....");
+
+        HelloWorld.TestMethod();
+    }
+}
